2cbf8663efc584ab7aa5fbb610e97e1b8078d95f
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2018-10-23 18:19:37 +0200

    Add project files.

---
+++ MultithreadGameLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadGameLoop
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Requirements:
+            //
+            // - Threads for user input, evaluation, rendering
+            //
+            // - Object-oriented approach to modeling the different threads
+            //
+            // - Configurable frequencies for each thread
+            //
+            // Optional:
+            //
+            // Secure, shared data management
+        }
+    }
+}
